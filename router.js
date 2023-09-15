2dec10e6c6906877b401f2b72becc0b846e0e927
Neeraj Goswami <ngoswami1978@users.noreply.github.com>
2023-09-15 16:20:04 +0530

    Login Page ejs using bootstrap

--- router.js
+++ router.js
@@ -10,58 +10,51 @@ const credential = {
 //Router.post() submits the data whereas router.get() requests the data.
 // login user
 router.post('/login', (req, res)=>{
-
     if(req.body.email == credential.email && req.body.password == credential.password){
 
         req.session.user = req.body.email;
-        res.redirect('/route/dashboard');
+        //res.redirect('/route/dashboard');
+        res.redirect('/route/index');        
         //res.end("Login Successful...!");
-
     }
     else{
         //res.end() function is used to end the response process.
         res.end("Invalid Username")
-
     }
 });
 
-
-
 // route for dashboard
 router.get('/dashboard', (req, res) => {
-
     if(req.session.user){
-
         res.render('dashboard', {user : req.session.user})
-
     }
     else{
-
         res.send("Unauthorize User")
-
     }
 })
 
+// route for index
+router.get('/index', (req, res) => {
+    if(req.session.user){
+        res.render('index', {user : req.session.user})
+    }
+    else{
+        res.send("Unauthorize User")
+    }
+})
 
 
 // route for logout
 router.get('/logout', (req ,res)=>{
-
     req.session.destroy(function(err){
-
         if(err){
-
             console.log(err);
             res.send("Error")
-
         }
         else{
-
             res.render('base', { title: "Express", logout : "logout Successfully...!"})
-
         }
     })
 })
-
 
 module.exports = router;
